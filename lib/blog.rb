ada15fee8ae5b3499478f5f88910ca93b8653ead
James Mead <james@floehopper.org>
2010-03-12 15:42:31 +0000

    Reverse the merge so that we actually pay attention to the options passed in.

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,7 +1,7 @@
 module BlogHelper
   
   def articles(options = {})
-    pages = @pages.find(:all, options.merge(:in_directory => '/blog', :sort_by => 'created_at', :reverse => true))
+    pages = @pages.find(:all, {:in_directory => '/blog', :sort_by => 'created_at', :reverse => true}.merge(options))
     pages.reject { |page| page.filename == 'index' }
   end
   
