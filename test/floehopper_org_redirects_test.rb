3516ed851aa1fbf8f4d15f6590d34a2665294c6f
James Mead <james@floehopper.org>
2010-05-31 10:44:50 +0100

    Added tests for article index and yearly,monthly & daily archives.

--- test/floehopper_org_redirects_test.rb
+++ test/floehopper_org_redirects_test.rb
@@ -30,5 +30,19 @@ class FloehopperOrgRedirectsTest < Test::Unit::TestCase
     assert_redirects blog_floehopper_org('/xml/rss/feed.xml'), feedburner_url
     assert_redirects blog_floehopper_org('/xml/rss20/feed.xml'), feedburner_url
   end
+  
+  def test_archives
+    assert_redirects blog_floehopper_org('/articles/2009/11/02/'), jamesmead_org('/archives')
+    assert_redirects blog_floehopper_org('/articles/2009/11/02'), jamesmead_org('/archives')
+    assert_redirects blog_floehopper_org('/articles/2009/11/'), jamesmead_org('/archives')
+    assert_redirects blog_floehopper_org('/articles/2009/11'), jamesmead_org('/archives')
+    assert_redirects blog_floehopper_org('/articles/2009/'), jamesmead_org('/archives')
+    assert_redirects blog_floehopper_org('/articles/2009'), jamesmead_org('/archives')
+  end
+  
+  def test_articles_index
+    assert_redirects blog_floehopper_org('/articles/'), jamesmead_org('/blog/')
+    assert_redirects blog_floehopper_org('/articles'), jamesmead_org('/blog/')
+  end
 
 end
