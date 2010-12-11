ae928e25db008dba3e02cb001f03d973e4476761
James Mead <james@floehopper.org>
2010-12-11 21:41:12 +0000

    Use the new static_website puppet recipe thingy and change webby config to deploy code to /var/apps to match what puppet is doing.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ server 'argonaut.slice', :app, :web, :db, :primary => true
 set :environment, 'production'
 
 manifest :app, %{
-  floehopper::app {'jamesmead.org':
+  floehopper::static_website {'jamesmead.org':
     deploy_to => "<%= deploy_to %>",
     domain => 'jamesmead.org'
   }
