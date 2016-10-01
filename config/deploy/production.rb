a8eab3a64a85e14babc5bc0a5a00cd41d40291ef
James Mead <james@floehopper.org>
2016-10-01 17:16:44 +0100

    Delete all files in preparation for rewriting using Middleman

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-server 'argonaut.slice', :app, :web, :db, :primary => true
-
-manifest :app, %{
-  floehopper::webby_app {'jamesmead.org':
-    deploy_to => "<%= deploy_to %>",
-    domain => 'jamesmead.org'
-  }
-}
