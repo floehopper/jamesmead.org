6760dd64e5a3999f19726efa96f46e4d42fd9905
James Mead <james@floehopper.org>
2020-06-27 12:32:03 +0100

    Display webmentions on blog article pages
    Closely based on this code [1].
    
    [1]: https://github.com/sebastiandedeyne/sebastiandedeyne.com/blob/f9c19f78e7a7b57562059a62154f0c9d9641267b/assets/js/webmentions.js

---
+++ source/javascripts/webmentions.js
@@ -0,0 +1,70 @@
+const container = document.querySelector("[data-webmentions]");
+
+if (container) {
+  renderWebmentions(container);
+}
+
+async function renderWebmentions(container) {
+  const webmentions = await getWebmentions(container.dataset.webmentions);
+
+  if (webmentions.length === 0) {
+    return;
+  }
+
+  const list = document.createElement("ul");
+  list.className = "webmentions";
+
+  webmentions.forEach(webmention => {
+    list.appendChild(renderWebmention(webmention));
+  });
+
+  container.appendChild(list);
+}
+
+function getWebmentions(target) {
+  return fetch(`https://webmention.io/api/mentions.jf2?target=${target}`)
+    .then(response => response.json())
+    .then(data => data.children);
+}
+
+function renderWebmention(webmention) {
+  const action = {
+    "in-reply-to": "replied",
+    "like-of": "liked",
+    "repost-of": "reposted",
+    "mention-of": "mentioned"
+  }[webmention["wm-property"]];
+
+  const rendered = document.importNode(
+    document.getElementById("webmention-template").content,
+    true
+  );
+
+  function set(selector, attribute, value) {
+    rendered.querySelector(selector)[attribute] = value;
+  }
+
+  set(".webmention .author", "href", webmention.author.url || webmention.url);
+  set(".webmention .author-name", "textContent", webmention.author.name);
+  set(".webmention .action", "href", webmention.url);
+
+  const receivedAt = new Date(webmention["wm-received"]);
+  const timestamp = `${receivedAt.toLocaleDateString(undefined, {day: 'numeric', month: 'short', year: 'numeric'})
+} at ${receivedAt.toLocaleTimeString(undefined, {hour: 'numeric', minute: 'numeric'})}`
+
+  set(
+    ".webmention .action",
+    "textContent",
+    `${action} on ${timestamp}`
+  );
+
+  if (webmention.content) {
+    set(
+      ".webmention .content",
+      "innerHTML",
+      webmention.content.html || webmention.content.text
+    );
+  }
+
+  return rendered;
+}
