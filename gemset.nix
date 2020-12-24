98195f35187de5614b0183dd93023e7e6078fa80
James Mead <james@floehopper.org>
2020-12-24 15:55:49 +0000

    Update gemset.nix to include rake gem
    This should've been done as part of this commit [1].
    
    [1]: 4c00ed995b58b839a2d87035364a1dcb5e088d09

--- gemset.nix
+++ gemset.nix
@@ -313,6 +313,16 @@
     };
     version = "2.2.3";
   };
+  rake = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1iik52mf9ky4cgs38fp2m8r6skdkq1yz23vh18lk95fhbcxb6a67";
+      type = "gem";
+    };
+    version = "13.0.3";
+  };
   rb-fsevent = {
     groups = ["default"];
     platforms = [];
