abd51a5b1984374f5a8e06cbb4a23a724dff6f51
James Mead <james@floehopper.org>
2020-12-24 16:07:55 +0000

    Simplify logic in TitleHelper#title

--- helpers/title_helper.rb
+++ helpers/title_helper.rb
@@ -1,7 +1,5 @@
 module TitleHelper
   def title(page)
-    title = page.data.title
-    return title if title == 'James Mead'
-    [title, 'James Mead'].join(' - ')
+    [page.data.title, 'James Mead'].uniq.compact.join(' - ')
   end
 end
